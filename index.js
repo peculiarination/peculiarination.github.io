3af8ac7181ebd5148c0e2902100840562167ad29
Peculiarination <peculiarinationsstuff@gmail.com>
2021-06-16 21:15:25 +0800

    Fixed ALMOST EVERYTHING POGGGGG

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+document.getElementById("goldMined").innerHTML = "Loading!"
 localStorage = window.localStorage;
 
-if (localStorage.getItem('newUser') == 'null'){
-    localStorage.setItem('newUser', 1) 
+
+int = parseInt
+if (!localStorage.getItem('gold')){
     localStorage.setItem('gold', 0) 
     localStorage.setItem('goldPerClick', 1)
     localStorage.setItem('goldPerClickCost', 10)
 }
 
 var gameData = {
-    gold: localStorage.getItem('gold'),
-    goldPerClick: localStorage.getItem('goldPerClick'),
-    goldPerClickCost: localStorage.getItem('goldPerClickCost')
+    gold: int(localStorage.getItem('gold')),
+    goldPerClick: int(localStorage.getItem('goldPerClick')),
+    goldPerClickCost: int(localStorage.getItem('goldPerClickCost'))
   }
 
   window.mineGold = mineGold
   window.buyGoldPerClick = buyGoldPerClick
   
   function mineGold() {
-    gameData.gold += gameData.goldPerClick
+    gameData.gold += (gameData.goldPerClick)
     document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
   }
   
