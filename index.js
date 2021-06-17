9f40c9d09cb461446215aed910c0e6a4113eac28
Peculiarination <peculiarinationsstuff@gmail.com>
2021-06-17 11:06:04 +0800

    Seperated auto from click. Also added reset button

--- index.js
+++ index.js
@@ -7,30 +7,46 @@ if (!localStorage.getItem('gold')){
     localStorage.setItem('gold', 0) 
     localStorage.setItem('goldPerClick', 1)
     localStorage.setItem('goldPerClickCost', 10)
+    localStorage.setItem('goldPerSecond', 1)
+    localStorage.setItem('goldPerSecondCost', 10)
 }
+
+
 
 var gameData = {
     gold: int(localStorage.getItem('gold')),
     goldPerClick: int(localStorage.getItem('goldPerClick')),
-    goldPerClickCost: int(localStorage.getItem('goldPerClickCost'))
+    goldPerClickCost: int(localStorage.getItem('goldPerClickCost')),
+    goldPerSecond: int(localStorage.getItem('goldPerSecond')),
+    goldPerSecondCost: int(localStorage.getItem('goldPerSecondCost'))
   }
 
   window.mineGold = mineGold
-  window.buyGoldPerClick = buyGoldPerClick
+  window.buyGoldPerSecond = buyGoldPerSecond
+  window.reset = reset
   
   function mineGold() {
     gameData.gold += (gameData.goldPerClick)
     document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Autominer (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
   }
   
+  function buyGoldPerSecond() {
+    if (gameData.gold >= gameData.goldPerSecondCost) {
+      gameData.gold -= gameData.goldPerSecondCost
+      gameData.goldPerSecond *= 2
+      gameData.goldPerSecondCost *= 2
+      document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+      document.getElementById("perSecondUpgrade").innerHTML = "Upgrade Autominer (Currently Level " + gameData.goldPerSecond  + ") Cost: " + gameData.goldPerSecondCost + " Gold"
+    }
+  }
+
   function buyGoldPerClick() {
     if (gameData.gold >= gameData.goldPerClickCost) {
       gameData.gold -= gameData.goldPerClickCost
-      gameData.goldPerClick += 1
-      gameData.goldPerClickCost *= 2
+      gameData.goldPerClick *= 2
+      gameData.goldPerClickCost += gameData.goldPerClickCost * 2
       document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
-      document.getElementById("perClickUpgrade").innerHTML = "Upgrade Autominer (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
+      document.getElementById("perSecondUpgrade").innerHTML = "Upgrade Pickaxe (Currently Level " + gameData.goldPerClick + ") Cost: " + gameData.goldPerClickCost + " Gold"
     }
   }
 
@@ -38,23 +54,30 @@ var gameData = {
      gameData = {
         gold: 0,
         goldPerClick: 1,
-        goldPerClickCost: 10
+        goldPerClickCost: 10,
+        goldPerSecond: 1,
+        goldPerSecondCost:10
       }
       localStorage.clear()
       if (!localStorage.getItem('gold')){
         localStorage.setItem('gold', 0) 
         localStorage.setItem('goldPerClick', 1)
         localStorage.setItem('goldPerClickCost', 10)
+        localStorage.setItem('goldPerSecond', 1)
+        localStorage.setItem('goldPerSecondCost', 10)
         }
         
       
   }
   
   var mainGameLoop = window.setInterval(function() {
-    mineGold()
+    gameData.gold += (gameData.goldPerSecond)
+    document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
     localStorage.setItem('gold', gameData.gold) 
     localStorage.setItem('goldPerClick', gameData.goldPerClick)
     localStorage.setItem('goldPerClickCost', gameData.goldPerClickCost)
+    localStorage.setItem('goldPerSecond', gameData.goldPerSecond)
+    localStorage.setItem('goldPerSecondCost', gameData.goldPerSecondCost)
         
 
   }, 1000)
