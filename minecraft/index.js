0776d62d67db8ca4cf8885800b385cf17c0e601f
Peculiarination <peculiarinationsstuff@gmail.com>
2021-06-18 11:36:22 +0800

    ton of new changes

--- minecraft/index.js
+++ minecraft/index.js
@@ -12,7 +12,9 @@ tempData = {
     miners:0,
     minerCost:100,
     drills:0,
-    drillCost:500
+    drillCost:500,
+    tractors:0,
+    tractorCost:2000
   }
 
 
@@ -35,7 +37,9 @@ gameData = {
     miners:int(localStorage.getItem('miners')),
     minerCost:int(localStorage.getItem('minerCost')),
     drills:int(localStorage.getItem('drills')),
-    drillCost:int(localStorage.getItem('drillCost'))
+    drillCost:int(localStorage.getItem('drillCost')),
+    tractors:int(localStorage.getItem('tractors')),
+    tractorCost:int(localStorage.getItem('tractorCost'))
   
 }
 
@@ -43,6 +47,7 @@ gameData = {
   window.buyPickaxe = buyPickaxe
   window.buyMiner = buyMiner 
   window.buyDrill = buyDrill   
+  window.buyTractor = buyTractor
 
   function mineBlock() {
     gameData.blocks += (gameData.blocksPerClick)
@@ -82,16 +87,31 @@ gameData = {
     }
   }
 
+  function buyTractor(){
+    if (gameData.blocks >= gameData.tractorCost){
+        gameData.blocks -= gameData.tractorCost
+        gameData.blocksPerSecond += 20
+        gameData.blocksPerClick += 3
+        gameData.tractors += 1
+        gameData.tractorCost += Math.floor((gameData.tractorCost) / 3)
+        document.getElementById("tractors").innerHTML = String(gameData.tractors)
+        document.getElementById("tractorCost").innerHTML = String(gameData.tractorCost) + " Blocks"
+    }
+  }
+
   var mainGameLoop = window.setInterval(function() {
     gameData.blocks += gameData.blocksPerSecond
     document.getElementById("blocksMined").innerHTML = gameData.blocks + " blocks"
     document.getElementById("dps").innerHTML = "Blocks per Second: " + gameData.blocksPerSecond
+    document.getElementById("dpc").innerHTML = "Blocks per Click: " + gameData.blocksPerClick
     document.getElementById("pickaxes").innerHTML = String(gameData.pickaxes)
     document.getElementById("pickaxeCost").innerHTML = String(gameData.pickaxeCost) + " Blocks"
     document.getElementById("miners").innerHTML = String(gameData.miners)
     document.getElementById("minerCost").innerHTML = String(gameData.minerCost) + " Blocks"
     document.getElementById("drills").innerHTML = String(gameData.drills)
     document.getElementById("drillCost").innerHTML = String(gameData.drillCost) + " Blocks"
+    document.getElementById("tractors").innerHTML = String(gameData.tractors)
+    document.getElementById("tractorCost").innerHTML = String(gameData.tractorCost) + " Blocks"
   }, 1000)
 
   var saveGameLoop = window.setInterval(function() {
