adddd5e4f1d5581b6168ef0ae0d5e1517756ed8b
Peculiarination <peculiarinationsstuff@gmail.com>
2021-06-21 16:02:15 +0800

    Removed dsstore

--- minecraft/index.js
+++ minecraft/index.js
@@ -99,34 +99,6 @@ gameData = {
     }
   }
 
-  function cipher(str, amount) { //non-site
-    if (amount < 0) {
-      return caesarShift(str, amount + 26);
-    }
-  
-    var output = "";
-  
-    for (var i = 0; i < str.length; i++) {
-      var c = str[i];
-  
-      if (c.match(/[a-z]/i)) {
-        var code = str.charCodeAt(i);
-  
-        if (code >= 65 && code <= 90) {
-          c = String.fromCharCode(((code - 65 + amount) % 26) + 65);
-        }
-  
-        else if (code >= 97 && code <= 122) {
-          c = String.fromCharCode(((code - 97 + amount) % 26) + 97);
-        }
-      }
-  
-      output += c;
-    }
-  
-    return output;
-  };
-
   function reset(){
     localStorage.clear()
     window.location.reload()
@@ -141,9 +113,6 @@ gameData = {
     
   }
 
-  function exportsave(){
-    console.log(cipher(JSON.stringify(gameData), 18))
-  }
 
   var mainGameLoop = window.setInterval(function() {
     gameData.blocks += gameData.blocksPerSecond
